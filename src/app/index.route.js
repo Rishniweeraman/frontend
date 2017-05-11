5cd93297180e0dcbe89143a9b6369ad20d702e9a
harinifreecodecampcorder <harini.ramanayaka@my.sliit.lk>
2017-05-11 11:19:08 +0530

    Merge branch 'master' of E:\sliittttt\AF\Assignment\group_project\project\frontend with conflicts.

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,29 +1,11 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
-      .state('home', {
+    .state('home', {
       url: '/',
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
-    })
-      .state('assistantPharmacist', {
-      url: '/assistantPharmacist',
-      templateUrl: 'app/assistantPharmacist/assistantPharmacist.html',
-      controller: 'assistantPharmacistController',
-      controllerAs: 'assistantPharmacist'
-    })
-      .state('chiefPharmacist', {
-      url: '/chiefPharmacist',
-      templateUrl: 'app/chiefPharmacist/chiefPharmacist.html',
-      controller: 'chiefPharmacistController',
-      controllerAs: 'chiefPharmacist'
-    })
-      .state('doctor', {
-      url: '/doctor',
-      templateUrl: 'app/doctor/doctor.html',
-      controller: 'doctorController',
-      controllerAs: 'doctor'
     });
 
   $urlRouterProvider.otherwise('/');
